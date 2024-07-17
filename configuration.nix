80e8fc74730c4a9a2f678230b010e5c33988ebba
Alexander <Desench@proton.me>
2024-07-18 01:52:35 +0300

    better

--- configuration.nix
+++ configuration.nix
@@ -223,7 +223,7 @@ environment.systemPackages = with pkgs; [
   qalculate-gtk
   audio-recorder kazam
   spotify-cli-linux spotifywm
-  android-studio.canary
+  android-studio
 
   # Open with
   feh gthumb
