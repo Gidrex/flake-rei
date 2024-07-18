41c4958b493164f7d56d99bb0818a58e126b306f
Alexander <Desench@proton.me>
2024-07-18 16:33:51 +0300

    better

--- modules/android-studio/default.nix
+++ modules/android-studio/default.nix
@@ -1,6 +1,7 @@
 let
   nixpkgs-24-05 = import (fetchTarball {
     url = "https://nixos.org/channels/nixos-24.05/nixexprs.tar.xz";
+    sha256 = "1nanlslf1mdnnpxl7i3c7dkdz2dm4mbl6d6xc2w1bx21jgsk5g37";
   }) { };
 in
 {
