687cc8d7a16fac46da54b6d00bc9f8ae36b3de9e
Alexander <Desench@proton.me>
2024-07-18 19:20:56 +0300

    better

--- modules/neovim/config/mappings.lua
+++ modules/neovim/config/mappings.lua
@@ -53,7 +53,7 @@ vim.api.nvim_set_keymap('n', '<C-l>', '<cmd>lua require("harpoon.ui").nav_file(4
 
 -- Oil
 vim.api.nvim_set_keymap('n', '<leader>r', '<cmd>Oil<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>t', '<cmd>tabedit<CR><cmd>Oil<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>t', '<cmd>tabedit<cr>', { noremap = true, silent = true })
 
 
 -- Neorg
