bb75961f908471bbb4c39e064cfc164687586dd9
Alexander <Desench@proton.me>
2024-07-20 23:23:08 +0300

    better

--- configuration.nix
+++ configuration.nix
@@ -106,7 +106,7 @@ programs = {
   gamemode.enable = true;
   fish.enable = true;
   zsh.enable = true;
-  # command-not-found.enable = true;
+  command-not-found.enable = true;
 };
 
 # Screen lighting
