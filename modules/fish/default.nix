bd61a1fc7c41f3a560edfe3a10f5e58d1851b993
Alexander <Desench@proton.me>
2024-07-22 13:59:51 +0300

    better

--- modules/fish/default.nix
+++ modules/fish/default.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }: 
 {
   programs.starship.enableFishIntegration = false;
-  programs.enableFishIntegration = false;
+  programs.zellij.enableFishIntegration = false;
   programs.fish = {
     enable = true;
     plugins = [
