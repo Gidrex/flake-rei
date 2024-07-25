825dc7374004a4d08ae668b85c0b9236ca296cb1
Alexander <Desench@proton.me>
2024-07-25 11:38:05 +0300

    better

--- modules/nixpkgs-24.05/default.nix
+++ modules/nixpkgs-24.05/default.nix
@@ -10,5 +10,10 @@ in
 {
   environment.systemPackages = with nixpkgs-24-05; [
     android-studio
+    cmdline-tools-latest
+    build-tools-34-0-0
+    platform-tools
+    platforms-android-34
+    emulator
   ];
 }
