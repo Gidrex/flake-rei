425e2a2ef273d56b1997d64a7f688578d0a4de9f
Alexander <Desench@proton.me>
2024-07-25 11:09:53 +0300

    better

--- modules/nixpkgs-24.05/default.nix
+++ modules/nixpkgs-24.05/default.nix
@@ -1,8 +1,7 @@
 let
   nixpkgs-24-05 = import (fetchTarball {
     url = "https://nixos.org/channels/nixos-24.05/nixexprs.tar.xz";
-    # sha256 = "0gy2wvfwwi2jss5prhxq5c1rw321mi82c0mnki5m404j2zzzas6f";
-    sha256 = "5571fe5cebc7f865d63c7228e25532223a1ad7e422b772a4cf1d769e6f789297";
+    sha256 = "0w5kza4qrnlhsp1ls385zmf6cbkfwcxiriz69bi29zjhn2rl9gh5";
     }) { 
     system = "x86_64-linux"; 
     config.allowUnfree = true;
