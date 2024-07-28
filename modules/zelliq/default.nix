c4b6c3920a1dd6d26ee0f879e409e87195d26f50
Alexander <Desench@proton.me>
2024-07-28 05:30:00 +0300

    better

--- modules/zelliq/default.nix
+++ modules/zelliq/default.nix
@@ -4,7 +4,7 @@ programs.zellij = {
   enableFishIntegration = false;
   settings = { #TODO - make compambility with my neovim hotkeys
     session.hide_status_bar = true;
-    default_layout = "compact"; 
+    # default_layout = "compact"; 
     pane_frames = true;
   };
 };
