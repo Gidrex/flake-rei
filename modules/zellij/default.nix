0fb6402fc047a57151e12a4ad3f1fd4618f05b3b
Alexander <Desench@proton.me>
2024-08-04 00:18:27 +0300

    better

--- modules/zellij/default.nix
+++ modules/zellij/default.nix
@@ -1,55 +1,39 @@
-{ config, pkgs, ... }:
-
+{ pkgs, ... }:
 {
   programs.zellij = {
     enable = true;
     enableFishIntegration = false;
-    settings = {
-      session.hide_status_bar = true;
-      pane_frames = false;
-      mouse_mode = false;
+    settingsFile = "${pkgs.lib.fileToString ./zellij-config.kdl}";
+settings = ''
+      session.hide_status_bar = true
+      pane_frames = false
+      mouse_mode = false
 
-      theme = "Catppuccin";
+      theme = "Catppuccin"
       themes.Catppuccin = {
-        fg = "#D9E0EE";
-        bg = "#1E1E2E";
-        black = "#1E1E2E";
-        red = "#f0658f";
-        green = "#ABE9B3";
-        yellow = "#FAE3B0";
-        orange = "#FAB387";
-        blue = "#96CDFB";
-        magenta = "#DDB6F2";
-        cyan = "#89DCEB";
-        white = "#C3BAC6";
-        bright_black = "#585B70";
-        bright_red = "#F28FAD";
-        bright_green = "#ABE9B3";
-        bright_yellow = "#FAE3B0";
-        bright_blue = "#96CDFB";
-        bright_magenta = "#DDB6F2";
-        bright_cyan = "#89DCEB";
-        bright_white = "#D9E0EE";
-      };
-    };
-  };
-
-  home.file.".config/zellij/config.kdl".text = ''
-    keybinds {
-      normal {
-        "Ctrl+Shift+Up" {
-          run "scroll up"
-        }
-        "Ctrl+Shift+Down" {
-          run "scroll down"
-        }
-        "Ctrl+Shift+PageUp" {
-          run "scroll up page"
-        }
-        "Ctrl+Shift+PageDown" {
-          run "scroll down page"
-        }
+        fg = "#D9E0EE"
+        bg = "#1E1E2E"
+        black = "#1E1E2E"
+        red = "#f0658f"
+        green = "#ABE9B3"
+        yellow = "#FAE3B0"
+        orange = "#FAB387"
+        blue = "#96CDFB"
+        magenta = "#DDB6F2"
+        cyan = "#89DCEB"
+        white = "#C3BAC6"
+        bright_black = "#585B70"
+        bright_red = "#F28FAD"
+        bright_green = "#ABE9B3"
+        bright_yellow = "#FAE3B0"
+        bright_blue = "#96CDFB"
+        bright_magenta = "#DDB6F2"
+        bright_cyan = "#89DCEB"
+        bright_white = "#D9E0EE"
       }
-    }
-  '';
+    '';
+
+    # Include the external keybinds file
+    home.file.".config/zellij/config.kdl".text = builtins.readFile ~/.config/nixpkgs/zellij-config.kdl;
+  };
 }
