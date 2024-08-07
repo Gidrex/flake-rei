4eb69a839fcbe8d2461447ee117fb349b5e3c1b1
Alexander <Desench@proton.me>
2024-08-07 19:19:07 +0300

    better

--- configuration.nix
+++ configuration.nix
@@ -27,53 +27,16 @@ networking = {
   proxy.httpProxy = "http://127.0.0.1:8889";
   proxy.allProxy = "socks5://localhost:1089";
   hostName = "rei";
-  # networkmanager.enable = true;
+  networkmanager.enable = true;
   # networkmanager.dns = "systemd-resolved";
   useNetworkd = true;
   useDHCP = false;
   nameservers = [
     "45.90.28.0#242bc4.dns.nextdns.io"
-      "2a07:a8c0::#242bc4.dns.nextdns.io"
-      "45.90.30.0#242bc4.dns.nextdns.io"
-      "2a07:a8c1::#242bc4.dns.nextdns.io"
+    "2a07:a8c0::#242bc4.dns.nextdns.io"
+    "45.90.30.0#242bc4.dns.nextdns.io"
+    "2a07:a8c1::#242bc4.dns.nextdns.io"
   ];
-};
-systemd.network = {
-  wait-online.anyInterface = true;
-  netdevs.wireCat = {
-    netdevConfig = {
-      Name = "wireCat";
-      Kind = "bond";
-    };
-    bondConfig = {
-      Mode = "active-backup";
-      MIIMonitorSec = "1s";
-      PrimaryReselectPolicy = "better";
-    };
-  };
-  networks = {
-    "90-ethers" = {
-      bond = [ "wireCat" ];
-      matchConfig = {
-        Type = "ether";
-        Kind = "!*"; # fix docker
-      };
-    };
-    "90-wlans" = {
-      bond = [ "wireCat" ];
-      matchConfig.WLANInterfaceType = "station";
-    };
-    "90-wireCat" = {
-      name = "wireCat";
-      networkConfig = {
-        DHCP = "yes";
-        DNSSEC = "yes";
-        DNSOverTLS = "yes";
-      };
-      dhcpV4Config.UseDNS = false;
-      dhcpV6Config.UseDNS = false;
-    };
-  };
 };
 
 # Locale
