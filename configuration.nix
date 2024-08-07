04953a5b1fa84ec96edfb86d108c236aa0f44c28
Alexander <Desench@proton.me>
2024-08-07 16:51:58 +0300

    better

--- configuration.nix
+++ configuration.nix
@@ -31,10 +31,11 @@ networking = {
 };
 services.dnscrypt-proxy2 = {
   enable = true;
+  upstreamDefaults = true;
   settings = {
-    servers = [ "scaleway-fr" "google" "cloudflare" "quad9-dnscrypt-ip4-filter-pri" "quad9-dnscrypt-ip4-filter-alt" ];
+    server_names = [ "scaleway-fr" "google" "cloudflare" "quad9-dnscrypt-ip4-filter-pri" "quad9-dnscrypt-ip4-filter-alt" ];
     cache = true;
-    proxy = "http://127.0.0.1:8889";
+    proxy = "socks5://localhost:1089";
   };
 };
 
