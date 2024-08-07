6cb4b6586d679d727cee813a19a5d96cd71ada34
Alexander <Desench@proton.me>
2024-08-07 15:17:44 +0300

    better

--- modules/zellij/default.nix
+++ modules/zellij/default.nix
@@ -4,7 +4,7 @@
     enableFishIntegration = false;
     settings = {
       session.hide_status_bar = true;
-      simplified_ui = true; # disable noob mode
+      default_layout = "compact";
       pane_frames = false;
       mouse_mode = false;
       theme = "Catppuccin";
