9fe88d374e778058daf2c1f0ba52807705b3293d
Alexander <Desench@proton.me>
2024-08-24 16:44:31 +0300

    better

--- flake.nix
+++ flake.nix
@@ -6,11 +6,12 @@
     impermanence.url = "github:nix-community/impermanence"; # TODO
     home-manager.url = "github:nix-community/home-manager";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    catppuccin.url = "github:catppuccin/nix";
     flake-utils.url = "github:numtide/flake-utils";
     byedpi.url = "github:Gidrex/byedpi-nix";
   };
 
-  outputs = { nixpkgs, home-manager, byedpi, ... }: let
+  outputs = { nixpkgs, home-manager, catppuccin, byedpi, ... }: let
     system = "x86_64-linux";
     # pkgs = import nixpkgs { system = system; };
   in {
@@ -19,11 +20,17 @@
         system = system;
         modules = [
           ./configuration.nix
-          home-manager.nixosModules.home-manager
+          catppuccin.nixosModules.catppuccin
+          # home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.gidrex = { imports = [ ./home.nix ]; };
+            home-manager.users.gidrex = {
+              imports = [ 
+                ./home.nix
+                catppuccin.homeManagerModules.catppuccin
+              ];
+            };
           }
           {
             environment.systemPackages = [ byedpi.packages.${system}.default ];
