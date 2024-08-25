8ba4581f4aca96d031bbc083243eb832ebb6b4d2
Alexander <Desench@proton.me>
2024-08-25 11:28:37 +0300

    better

--- configuration.nix
+++ configuration.nix
@@ -1,296 +1,298 @@
 { config, pkgs, ... }: 
 {
-imports = [ 
-  ./hardware-configuration.nix
-  ./rice/i3
-];
+  imports = [ 
+    ./hardware-configuration.nix
+    ./rice/i3
+  ];
 
-security.polkit.enable = true;
-boot = {
-  binfmt.emulatedSystems = ["aarch64-linux"];
-  kernelPackages = pkgs.pkgs.linuxPackages_xanmod_latest;
-  loader = {
-    systemd-boot.enable = false;
-    efi.canTouchEfiVariables = true;
-    systemd-boot.configurationLimit = 20;
-    timeout = 5;
-    grub.enable = true;
-    grub.efiSupport = true;
-    grub.device = "nodev";
-  };
-};
-nix.settings.experimental-features = ["nix-command" "flakes"];
-nix.gc.automatic = true; #Garbage collector
-
-# Networking
-networking = {
-  proxy.httpProxy = "http://127.0.0.1:8889";
-  proxy.allProxy = "socks5://localhost:1089";
-  hostName = "rei";
-  networkmanager.enable = true;
-};
-
-# Locale
-time.timeZone = "Europe/Moscow";
-i18n.defaultLocale = "en_GB.UTF-8";
-
-# Audio
-# sound.enable = true;
-services.pipewire = {
-  enable = true;
-  alsa.enable = true;
-  pulse.enable = true;
-  jack.enable = true;
-};  
-hardware.bluetooth.enable = true;
-
- # User
-users.users.gidrex = {
-  shell = pkgs.fish;
-  isNormalUser = true;
-  description = "Alexander";
-  extraGroups = [ "networkmanager" "wheel" "audio" "input" /* "docker" */ "vboxusers" "input" "plugdev" "libvirtd" "kvm"];
-};
-
-# Graphics
-boot.kernelModules = ["nvidia" "i2c-dev" "nvidia-drm" "nvidia-modeset" "nvidia-uvm"];
-services.xserver.videoDrivers = ["nvidia"];
-hardware = {
-  graphics = {
-    enable = true;
-    enable32Bit = true;
-    extraPackages = with pkgs; [ vaapiVdpau libvdpau-va-gl mesa.drivers ];
-  };
-  nvidia = {
-    open = false;
-    nvidiaSettings = true;
-    powerManagement.enable = true;
-    powerManagement.finegrained = true;
-    dynamicBoost.enable = true;
-    modesetting.enable = true;
-    package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
-    prime = {
-      offload.enable = true;
-      offload.enableOffloadCmd = true;
-      intelBusId =  "PCI:00:02:0";
-      nvidiaBusId = "PCI:01:00:0";
+  security.polkit.enable = true;
+  boot = {
+    binfmt.emulatedSystems = ["aarch64-linux"];
+    kernelPackages = pkgs.pkgs.linuxPackages_xanmod_latest;
+    loader = {
+      systemd-boot.enable = false;
+      efi.canTouchEfiVariables = true;
+      systemd-boot.configurationLimit = 20;
+      timeout = 5;
+      grub.enable = true;
+      grub.efiSupport = true;
+      grub.device = "nodev";
     };
   };
-};
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+  nix.gc.automatic = true; #Garbage collector
 
-# Docker
-# virtualisation.docker.enable = true;
-# virtualisation.docker.rootless = {
-#   enable = true;
-#   setSocketVariable = true;
-# };
-# virtualisation.containers.enable = true;
-# users.extraGroups.docker.members = [ "username-with-access-to-socket" ];
-
-programs = {
-  thunar.enable = true;
-  thunar.plugins = with pkgs.xfce; [ thunar-archive-plugin thunar-media-tags-plugin ];
-  steam.enable = true;
-  gamescope.enable = true;
-  gamemode.enable = true;
-  fish.enable = true;
-  zsh.enable = true;
-  command-not-found.enable = false;
-};
-
-# Screen lighting
-programs.light.enable = true;
-systemd.services.chmodBacklight = {
-  description = "Set permissions for backlight brightness control";
-  after = [ "network.target" ];
-  wantedBy = [ "multi-user.target" ];
-  serviceConfig = {
-    Type = "oneshot";
-    ExecStart = "/run/current-system/sw/bin/chmod a+wr /sys/class/backlight/intel_backlight/brightness";
-    RemainAfterExit = true;
+  # Networking
+  networking = {
+    proxy.httpProxy = "http://127.0.0.1:8889";
+    proxy.allProxy = "socks5://localhost:1089";
+    hostName = "rei";
+    networkmanager.enable = true;
   };
-};
 
-# Services
-services = {
-  openssh.enable = true;
-  udisks2.enable = true; # auto mount flash usb
-  udisks2.mountOnMedia = true;
-  deluge.enable = true;
-  printing.enable = true;
-  printing.drivers = with pkgs; [ pantum-driver ];
-  blueman.enable = true;
-  onlyoffice.enable = true;
-};
+  # Locale
+  time.timeZone = "Europe/Moscow";
+  i18n.defaultLocale = "en_GB.UTF-8";
 
-# Fonts
-fonts.fontconfig.enable = true;
-fonts.packages = with pkgs; [
-  nerdfonts
-  noto-fonts
-  jetbrains-mono
-  _0xproto
-  times-newer-roman
-];
+  # Audio
+  # sound.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };  
+  hardware.bluetooth.enable = true;
 
-# Packets
-nixpkgs.config.allowUnfree = true;
-environment.systemPackages = with pkgs; [
-# --------------- Terminal tools --------------- #
-  # musthave
-  neovim tldr
-  pciutils gnutls usbutils util-linux wget
-  acpi sysstat
-  htop
-  coreutils-prefixed pinentry
-
-  # term tools
-  xsel ffmpeg
-  file hexyl procs broot chafa dig
-  imagemagick
-  fd ripgrep procs jq 
-  nix-inspect nix-prefetch-github
-
-  # web & security
-  openssl
-  cloak pass
-  shadowsocks-rust shadowsocks-libev 
-  wireguard-tools udp2raw inetutils v2ray qv2ray
-  crow-translate
-  ytfzf yt-dlp
-  git-filter-repo
-
-  # fun
-  neofetch
-
-  # university learning & documents
-  zip unzip
-  pandoc
-
-  # dependencies
-  alsa-utils ueberzug
-
-  # why Im garbage collector?
-  micro
-
-  # I want to learn it
-  lynx elinks
-# ---------------------------------------------- #
-
-  # Shells
-  kitty fish alacritty nushell starship
-
-  # Dev
-  gradle openjdk jdk8 #libcanberra 
-  python3 python312Packages.pip virtualenv python3Packages.psutil
-  python312Packages.huggingface-hub python312Packages.tkinter
-  gdb
-  lua 
-  nodejs yarn deno
-  rustup cargo rust-script
-  flutter dart
-  gcc cmake gnumake pkg-config freetype
-  libstdcxx5 
-  sqlite sqlite-utils
-
-  # Utility
-  flameshot pick-colour-picker
-
-  # Apps
-  firefox ungoogled-chromium 
-  libsForQt5.dolphin libsForQt5.qtstyleplugin-kvantum libsForQt5.qt5ct
-  webtorrent_desktop
-  telegram-desktop vesktop 
-  onlyoffice-bin_latest
-  logseq figma-linux
-  androidStudioPackages.canary
-  mysql-workbench dbeaver-bin sqlitebrowser
-  drawio gimp krita
-  audio-recorder kazam obs-studio
-  spotify-cli-linux spotifywm
-  scanmem
-  anydesk
-
-  # Open with
-  feh gthumb
-  evince
-  mpv
-  qimgv
-  ark
-
-  # Boxes
-  fuse wine winetricks
-  protonup protontricks
-  lutris
-  distrobox podman
-  qemu_kvm libvirt bridge-utils
-
-  # Rice
-  xorg.xf86videofbdev xorg.xkbcomp
-  lxappearance nitrogen rofi dmenu fzf pavucontrol
-
-  # Drivers
-  blueman playerctl
-  vulkan-validation-layers
-  cloudflared
-  vial qmk # for ergonomic keyboard
-  udisks2 udiskie # auto mount usb flash 
-  cups pantum-driver cups-printers # for my printer
-  xarchive
-  vulkan-tools vulkan-loader vulkan-validation-layers
-  dotnetCorePackages.sdk_6_0_1xx
-  acpid
-
-  # Dependencies
-  xorg.libX11
-  xorg.libXfixes
-  xorg.libXrandr
-  xorg.xmodmap
-
-  # Gaming
-  mangohud
-];
-
-# Electron <3 .!.
-nixpkgs.config.permittedInsecurePackages = [ "electron-27.3.11" ];
-
-# xserver conf
-services.xserver = {
-  enable = true;
-  xkb = {
-    layout = "us,ru";
-    model = "pc105";
-    options = "grp:alt_shift_toggle";
+  # User
+  users.users.gidrex = {
+    shell = pkgs.fish;
+    isNormalUser = true;
+    description = "Alexander";
+    extraGroups = [ "networkmanager" "wheel" "audio" "input" /* "docker" */ "vboxusers" "input" "plugdev" "libvirtd" "kvm"];
   };
-  displayManager.lightdm = {
-    background = ./assets/nix.png;
-    enable  = true;
+
+  # Graphics
+  boot.kernelModules = ["nvidia" "i2c-dev" "nvidia-drm" "nvidia-modeset" "nvidia-uvm"];
+  services.xserver.videoDrivers = ["nvidia"];
+  hardware = {
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+      extraPackages = with pkgs; [ vaapiVdpau libvdpau-va-gl mesa.drivers ];
+    };
+    nvidia = {
+      open = false;
+      nvidiaSettings = true;
+      powerManagement.enable = true;
+      powerManagement.finegrained = true;
+      dynamicBoost.enable = true;
+      modesetting.enable = true;
+      package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
+      prime = {
+        offload.enable = true;
+        offload.enableOffloadCmd = true;
+        intelBusId =  "PCI:00:02:0";
+        nvidiaBusId = "PCI:01:00:0";
+      };
+    };
   };
-};
-environment.variables.PATH = "${pkgs.stdenv.cc}/bin:/usr/bin";
 
-# flatpak
-services.flatpak.enable = true;
-xdg.portal = {
-  enable = true; 
-  extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
-  config.common.default = "*";
-};
+  # Docker
+  # virtualisation.docker.enable = true;
+  # virtualisation.docker.rootless = {
+  #   enable = true;
+  #   setSocketVariable = true;
+  # };
+  # virtualisation.containers.enable = true;
+  # users.extraGroups.docker.members = [ "username-with-access-to-socket" ];
 
-# ergonomic keyboard
-hardware.keyboard.qmk.enable = true;
-services.udev.extraRules = ''
+  programs = {
+    thunar.enable = true;
+    thunar.plugins = with pkgs.xfce; [ thunar-archive-plugin thunar-media-tags-plugin ];
+    steam.enable = true;
+    gamescope.enable = true;
+    gamemode.enable = true;
+    fish.enable = true;
+    zsh.enable = true;
+    command-not-found.enable = false;
+  };
+
+  # Screen lighting
+  programs.light.enable = true;
+  systemd.services.chmodBacklight = {
+    description = "Set permissions for backlight brightness control";
+    after = [ "network.target" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "/run/current-system/sw/bin/chmod a+wr /sys/class/backlight/intel_backlight/brightness";
+      RemainAfterExit = true;
+    };
+  };
+
+  # Services
+  services = {
+    openssh.enable = true;
+    udisks2.enable = true; # auto mount flash usb
+    udisks2.mountOnMedia = true;
+    deluge.enable = true;
+    printing.enable = true;
+    printing.drivers = with pkgs; [ pantum-driver ];
+    blueman.enable = true;
+    onlyoffice.enable = true;
+  };
+
+  # Fonts
+  fonts.fontconfig.enable = true;
+  fonts.packages = with pkgs; [
+    nerdfonts
+    noto-fonts
+    jetbrains-mono
+    _0xproto
+    times-newer-roman
+  ];
+
+  # Packets
+  nixpkgs.config.allowUnfree = true;
+  environment.systemPackages = with pkgs; [
+    # --------------- Terminal tools --------------- #
+    # musthave
+    neovim tldr
+    pciutils gnutls usbutils util-linux wget
+    acpi sysstat
+    htop
+    coreutils-prefixed pinentry
+
+    # term tools
+    xsel ffmpeg
+    file hexyl procs broot chafa dig
+    imagemagick
+    fd ripgrep procs jq 
+    nix-inspect nix-prefetch-github
+
+    # web & security
+    openssl
+    cloak pass
+    shadowsocks-rust shadowsocks-libev 
+    wireguard-tools udp2raw inetutils v2ray qv2ray
+    crow-translate
+    ytfzf yt-dlp
+    git-filter-repo
+
+    # fun
+    neofetch
+
+    # university learning & documents
+    zip unzip
+    pandoc
+
+    # dependencies
+    alsa-utils ueberzug
+
+    # why Im garbage collector?
+    micro
+
+    # I want to learn it
+    lynx elinks
+    # ---------------------------------------------- #
+
+    # Shells
+    kitty fish alacritty nushell starship
+
+    # Dev
+    gradle openjdk jdk8 #libcanberra 
+    python3 python312Packages.pip virtualenv python3Packages.psutil
+    python312Packages.huggingface-hub python312Packages.tkinter
+    gdb
+    lua 
+    nodejs yarn deno
+    rustup cargo rust-script
+    flutter dart
+    gcc cmake gnumake pkg-config freetype
+    libstdcxx5 
+    sqlite sqlite-utils
+
+    # Utility
+    flameshot pick-colour-picker
+
+    # Apps
+    firefox ungoogled-chromium 
+    libsForQt5.dolphin libsForQt5.qtstyleplugin-kvantum libsForQt5.qt5ct
+    webtorrent_desktop
+    telegram-desktop vesktop 
+    onlyoffice-bin_latest
+    logseq figma-linux
+    androidStudioPackages.canary
+    mysql-workbench dbeaver-bin sqlitebrowser
+    drawio gimp krita
+    audio-recorder kazam obs-studio
+    spotify-cli-linux spotifywm
+    scanmem
+    anydesk
+
+    # Open with
+    feh gthumb
+    evince
+    mpv
+    qimgv
+    ark
+
+    # Boxes
+    fuse wine winetricks
+    protonup protontricks
+    lutris
+    distrobox podman
+    qemu_kvm libvirt bridge-utils
+
+    # Rice
+    xorg.xf86videofbdev xorg.xkbcomp
+    lxappearance nitrogen rofi dmenu fzf pavucontrol
+
+    # Drivers
+    blueman playerctl
+    vulkan-validation-layers
+    cloudflared
+    vial qmk # for ergonomic keyboard
+    udisks2 udiskie # auto mount usb flash 
+    cups pantum-driver cups-printers # for my printer
+    xarchive
+    vulkan-tools vulkan-loader vulkan-validation-layers
+    dotnetCorePackages.sdk_6_0_1xx
+    acpid
+
+    # Dependencies
+    xorg.libX11
+    xorg.libXfixes
+    xorg.libXrandr
+    xorg.xmodmap
+
+    # Gaming
+    mangohud
+  ];
+
+  # Electron <3 .!.
+  nixpkgs.config.permittedInsecurePackages = [ "electron-27.3.11" ];
+
+  # xserver conf
+  services.xserver = {
+    enable = true;
+    xkb = {
+      layout = "us,ru";
+      model = "pc105";
+      options = "grp:alt_shift_toggle";
+    };
+    displayManager.lightdm = {
+      background = ./assets/nix.png;
+      enable  = true;
+    };
+  };
+  environment.variables.PATH = "${pkgs.stdenv.cc}/bin:/usr/bin";
+
+  # flatpak
+  services.flatpak.enable = true;
+  xdg.portal = {
+    enable = true; 
+    extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
+    config.common.default = "*";
+  };
+
+  # ergonomic keyboard
+  hardware.keyboard.qmk.enable = true;
+  services.udev.extraRules = ''
   SUBSYSTEM=="usb", ATTR{idVendor}=="0x1038", GROUP="plugdev", MODE="0666"
-'';
+  '';
 
-# Looking glass TODO
-virtualisation.libvirtd = {
-  enable = true;
-  qemu.package = pkgs.qemu_kvm;
-  qemu.ovmf.enable = true;
-};
-programs.virt-manager.enable = true;
-services.dbus.packages = [ pkgs.dbus ];
+  # Looking glass TODO
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu.package = pkgs.qemu_kvm;
+    qemu.ovmf.enable = true;
+  };
+  programs.virt-manager.enable = true;
+  services.dbus.packages = [ pkgs.dbus ];
 
-system.stateVersion = "24.11";
+  environment.variables.QT_QPA_PLATFORMTHEME = "qt5ct";
+
+  system.stateVersion = "24.11";
 }
--- home.nix
+++ home.nix
@@ -1,139 +1,136 @@
 {  pkgs, ... }:
 {
-imports = [
-  ./modules/neovim
-  ./modules/tmux
-  ./modules/fish
-  ./modules/zellij
-  # ./rice/hyprland
-  # ./rice/sway
-]; 
+  imports = [
+    ./modules/neovim
+    ./modules/tmux
+    ./modules/fish
+    ./modules/zellij
+    # ./rice/hyprland
+    # ./rice/sway
+  ]; 
 
-home.packages = with pkgs; [
-  # theming
-  tokyo-night-gtk
-  catppuccin
+  home.packages = with pkgs; [
+    # theming
+    tokyo-night-gtk
+    catppuccin
+    catppuccin-kvantum
 
-  # Apps
-  spicetify-cli spotifywm
+    # Apps
+    spicetify-cli spotifywm
 
-  # LSP Servers
-  pyright
-  cmake-language-server
-  nil
-  rust-analyzer
-  ansible-language-server
-  nodePackages_latest.dockerfile-language-server-nodejs
-  nodePackages.vim-language-server
-  lua-language-server
-  buf-language-server
-];
+    # LSP Servers
+    pyright
+    cmake-language-server
+    nil
+    rust-analyzer
+    ansible-language-server
+    nodePackages_latest.dockerfile-language-server-nodejs
+    nodePackages.vim-language-server
+    lua-language-server
+    buf-language-server
+  ];
 
-home.sessionVariables = {
-  EDITOR = "nvim";
-  VISUAL = "nvim";
-  TERM = "alacritty";
-};
-
-# Theming
-catppuccin.enable = true;
-catppuccin.flavor = "mocha";
-catppuccin.accent = "lavender";
-qt.style.name = "kvantum";
-qt.style.package = pkgs.catppuccin-kvantum;
-# qt.style.catppuccin.enable = true;
-
-# excludes
-programs.zellij.catppuccin.enable = false;
-
-programs = {
-  firefox.enable = true;
-  gpg.enable = true;
-  zoxide.enable = true;
-  alacritty.enable = true;
-  alacritty.catppuccin.enable = false;
-  mangohud.enable = true;
-  # looking-glass-client.enable = true;
-  yazi.enable = true;
-  lazygit.enable = true;
-  btop.enable = true;
-  direnv.enable = true;
-  environment.variables.QT_QPA_PLATFORMTHEME = "qt5ct";
-
-  fzf = {
-    enable = true;
-    # defaultOptions = [ "--preview 'bat {}'" ];
-    defaultCommand = "fd --type f";
+  home.sessionVariables = {
+    EDITOR = "nvim";
+    VISUAL = "nvim";
+    TERM = "alacritty";
   };
 
-  pandoc = {
-    enable = true;
-    defaults.metadata.author = "Alexander";
-    defaults.pdf-engine = "xelatex";
-    defaults.citeproc = true;
-  };
+  # Theming
+  catppuccin.enable = true;
+  catppuccin.flavor = "mocha";
+  catppuccin.accent = "lavender";
 
-  eza = {
-    enable = true;
-    icons = true;
-    git = true;
-    extraOptions = [ "--oneline" "--group-directories-first" ];
-  };
+  # excludes
+  programs.zellij.catppuccin.enable = false;
 
-  bat = {
-    enable = true;
-    config.style = "numbers";
-    # config.theme = "Nord";
-    config.pager = "less -FR";
-    config.color = "always";
-  };
+  programs = {
+    firefox.enable = true;
+    gpg.enable = true;
+    zoxide.enable = true;
+    alacritty.enable = true;
+    alacritty.catppuccin.enable = false;
+    mangohud.enable = true;
+    # looking-glass-client.enable = true;
+    yazi.enable = true;
+    lazygit.enable = true;
+    btop.enable = true;
+    direnv.enable = true;
 
-  git = {
-    enable = true;
-    userName = "Alexander";
-    userEmail = "Desench@proton.me";
-    extraConfig = {
-      init.defaultBranch = "main";
-      pull.rebase = true;
-      rebase.autostash = true;
-      rebase.autosquash = true;
-      push.autoSetupRemote = true;
-      commit.gpgsign = false;
-      rerere.enabled = true;
-      gpg.format = "ssh";
-      user.signingkey = "~/.ssh/id_rsa.pub";
-      core.whitespace = "trailing-space,space-before-tab";
-      core.editor = "nvim";
+    fzf = {
+      enable = true;
+      # defaultOptions = [ "--preview 'bat {}'" ];
+      defaultCommand = "fd --type f";
+    };
+
+    pandoc = {
+      enable = true;
+      defaults.metadata.author = "Alexander";
+      defaults.pdf-engine = "xelatex";
+      defaults.citeproc = true;
+    };
+
+    eza = {
+      enable = true;
+      icons = true;
+      git = true;
+      extraOptions = [ "--oneline" "--group-directories-first" ];
+    };
+
+    bat = {
+      enable = true;
+      config.style = "numbers";
+      # config.theme = "Nord";
+      config.pager = "less -FR";
+      config.color = "always";
+    };
+
+    git = {
+      enable = true;
+      userName = "Alexander";
+      userEmail = "Desench@proton.me";
+      extraConfig = {
+        init.defaultBranch = "main";
+        pull.rebase = true;
+        rebase.autostash = true;
+        rebase.autosquash = true;
+        push.autoSetupRemote = true;
+        commit.gpgsign = false;
+        rerere.enabled = true;
+        gpg.format = "ssh";
+        user.signingkey = "~/.ssh/id_rsa.pub";
+        core.whitespace = "trailing-space,space-before-tab";
+        core.editor = "nvim";
+      };
     };
   };
-};
 
-services = {
-  spotifyd = {
-    enable = true;
-    settings.global = {
-      device_name = "nix";
-      username = "Gidrex";
-      password_cmd = "pass show spotifyd";
-      backend = "alsa";
-      dbus_type = "session"; # "system" for no graphic
+  services = {
+    spotifyd = {
+      enable = true;
+      settings.global = {
+        device_name = "nix";
+        username = "Gidrex";
+        password_cmd = "pass show spotifyd";
+        backend = "alsa";
+        dbus_type = "session"; # "system" for no graphic
         bitrate = 320;
-      volume_normalisation = true;
-      autoplay = true;
-      proxy = "http://127.0.0.1:8889";
+        volume_normalisation = true;
+        autoplay = true;
+        proxy = "http://127.0.0.1:8889";
+      };
+    };
+
+    udiskie = {
+      enable = true;
+      tray = "auto";
+      automount = true;
     };
   };
 
-  udiskie = {
-    enable = true;
-    tray = "auto";
-    automount = true;
-  };
-};
-
-programs.home-manager.enable = true;
-home.username = "gidrex";
-home.homeDirectory = "/home/gidrex";
-home.enableNixpkgsReleaseCheck = false;
-home.stateVersion = "24.05";
+  programs.home-manager.enable = true;
+  home.username = "gidrex";
+  home.homeDirectory = "/home/gidrex";
+  home.enableNixpkgsReleaseCheck = false;
+  home.stateVersion = "24.05";
 }
