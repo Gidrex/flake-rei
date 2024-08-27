1e4d26f8e5897d340869dad29ff448816394ceb4
Alexander <Desench@proton.me>
2024-08-28 00:09:52 +0300

    better

--- home.nix
+++ home.nix
@@ -1,36 +1,14 @@
-{  pkgs, config, ... }:
-let
-  androidSdkModule = import ((builtins.fetchGit {
-    url = "https://github.com/tadfisher/android-nixpkgs.git";
-    ref = "main";  # Or "stable", "beta", "preview", "canary"
-  }) + "/hm-module.nix");
-
-in
+{  pkgs, ... }:
 {
   imports = [
     ./modules/neovim
     ./modules/tmux
     ./modules/fish
     ./modules/zellij
-    # ./modules/android-sdk
+    ./modules/android-sdk
     # ./rice/hyprland
-    androidSdkModule
     # ./rice/sway
   ]; 
-
-
-  android-sdk.enable = true;
-
-  # Optional; default path is "~/.local/share/android".
-  android-sdk.path = "${config.home.homeDirectory}/.android/sdk";
-
-  android-sdk.packages = sdkPkgs: with sdkPkgs; [
-    build-tools-34-0-0
-    cmdline-tools-latest
-    emulator
-    platforms-android-34
-    sources-android-34
-  ];
 
   home.packages = with pkgs; [
     # theming
