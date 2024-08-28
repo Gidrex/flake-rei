2b363722dbfe500f77b4ff24dd0867b606ae6f9f
Alexander <Desench@proton.me>
2024-08-28 20:18:18 +0300

    better

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
     byedpi.url = "github:Gidrex/byedpi-nix";
   };
 
-  outputs = { nixpkgs, home-manager, catppuccin, byedpi, android-nixpkgs, ... }: let
+  outputs = { nixpkgs, home-manager, catppuccin, byedpi, ... }: let
     system = "x86_64-linux";
     # pkgs = import nixpkgs { system = system; };
   in {
@@ -37,23 +37,6 @@
               imports = [
                 ./home.nix
                 catppuccin.homeManagerModules.catppuccin
-
-                android-nixpkgs.hmModule
-                {
-                  inherit config lib pkgs;
-                  android-sdk.enable = true;
-
-                  # Optional; default path is "~/.local/share/android".
-                  android-sdk.path = "${config.home.homeDirectory}/.android/sdk";
-
-                  android-sdk.packages = sdk: with sdk; [
-                    build-tools-34-0-0
-                    cmdline-tools-latest
-                    emulator
-                    platforms-android-34
-                    sources-android-34
-                  ];
-                }
               ];
             };
           }
