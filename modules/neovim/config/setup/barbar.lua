49a5e6dabcfe34c57eba902f3f5b53eff8a82b96
Alexander <Desench@proton.me>
2024-09-07 15:32:04 +0300

    better

--- modules/neovim/config/setup/barbar.lua
+++ modules/neovim/config/setup/barbar.lua
@@ -6,11 +6,8 @@ require'barbar'.setup {
     },
     init = function() vim.g.barbar_auto_setup = false end,
     opts = {
-      -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
-      -- animation = true,
-      -- insert_at_start = true,
-      -- …etc.
+      animation = false,
     },
-    version = '^1.0.0', -- optional: only update when a new 1.x version is released
+    -- version = '^1.0.0', -- optional: only update when a new 1.x version is released
   },
 }
