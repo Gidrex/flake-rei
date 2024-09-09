38e6b024f6b89c0920a520b72822c74983fdfc03
Alexander <Desench@proton.me>
2024-09-09 13:16:25 +0300

    better

--- modules/neovim/config/options.lua
+++ modules/neovim/config/options.lua
@@ -28,7 +28,6 @@ vim.opt.colorcolumn = '0' -- was 100
 vim.o.encoding = 'utf-8'
 vim.o.fileencoding = 'utf-8'
 vim.o.fileencodings = 'utf-8,cp1251,koi8-r,latin1'
-vim.opt.langmap = "АA,БB,ВV,ГG,ДD,ЕE,ЁYo,ЖZh,ЗZ,ИI,ЙY,КK,ЛL,МM,НN,ОO,ПP,РR,СS,ТT,УU,ФF,ХKh,ЦTs,ЧCh,ШSh,ЩShch,Ъ\",ЫY,Ь\',ЭE,ЮYu,ЯYa"
 
 vim.cmd([[
 set completeopt=menu,menuone,noselect
