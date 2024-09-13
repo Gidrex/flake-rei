af0d777b88c4944f7e63cf1d9f866b713f128166
Alexander <Desench@proton.me>
2024-09-13 22:41:16 +0300

    better

--- modules/neovim/config/options.lua
+++ modules/neovim/config/options.lua
@@ -52,4 +52,4 @@ vim.diagnostic.config({
 -- You will likely want to reduce updatetime which affects CursorHold
 -- note: this setting is global and should be set only once
 vim.o.updatetime = 500
-vim.cmd [[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
+-- vim.cmd [[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
