d28bb44fc716abda0a0e2c8f1147b541ec81863a
Alexander <Desench@proton.me>
2024-09-18 00:38:44 +0300

    better

--- configuration.nix
+++ configuration.nix
@@ -104,7 +104,7 @@
     zsh.enable = true;
     command-not-found.enable = false;
     nh = {
-      flake = "~/flake-rei";
+      flake = "${builtins.getHome}/flake-rei";
       enable = true;
       clean.enable = true;
       clean.extraArgs = "--keep 5 --keep-since 7d"; # instead of nix.gc.automatic = true;
