cfed61db48b733ea87cf385b2fc261d4faffe5f1
Alexander <Desench@proton.me>
2024-09-21 22:28:17 +0300

    better

--- modules/yazi/default.nix
+++ modules/yazi/default.nix
@@ -1,21 +1,26 @@
 { pkgs, ... }:
+
+let
+  yaziPluginsRep = pkgs.fetchFromGitHub {
+    owner = "yazi-rs";
+    repo = "plugins";
+    rev = "49137feda8e140ebd7870292030d89c221cacce8";
+    sha256 = "3F7RIg2CZH/jo+XhG0n4Zfspgi/77Hve421j0p3Og+Q=";
+  };
+in
 {
   programs.yazi = {
     enable = true;
     shellWrapperName = "y";
 
     plugins = {
-      full-border = pkgs.fetchFromGitHub {
-        owner = "yazi-rs";
-        repo = "plugins";
-        rev = "main";
-        sha256 = "3F7RIg2CZH/jo+XhG0n4Zfspgi/77Hve421j0p3Og+Q=";
-      } + "/full-border.yazi";
+      full-border = yaziPluginsRep + "/full-border.yazi";
+      max-preview = yaziPluginsRep + "/max-preview.yazi";
     };
 
     initLua = ''
       -- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
-        require("full-border"):setup { type = ui.Border.PLAIN, }
+        require("full-border"):setup { type = ui.Border.ROUNDED, }
     '';
   };
 }
