e80c97d12babed1372dddc9bb7dc2f4a42eadca8
Alexander <Desench@proton.me>
2024-09-21 22:34:07 +0300

    better

--- modules/yazi/default.nix
+++ modules/yazi/default.nix
@@ -7,21 +7,24 @@ let
     rev = "49137feda8e140ebd7870292030d89c221cacce8";
     sha256 = "3F7RIg2CZH/jo+XhG0n4Zfspgi/77Hve421j0p3Og+Q=";
   };
+
+  pluginPath = pluginName: yaziPluginsRep + "/${pluginName}.yazi";
+
 in
-  {
+{
   programs.yazi = {
     enable = true;
     shellWrapperName = "y";
 
     plugins = {
-      full-border = yaziPluginsRep + "/full-border.yazi";
-      max-preview = yaziPluginsRep + "/max-preview.yazi";
+      full-border = pluginPath "full-border";
+      max-preview = pluginPath "max-preview";
     };
 
     initLua = ''
-      -- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
-        require("full-border"):setup { type = ui.Border.ROUNDED, }
+      require("full-border"):setup { type = ui.Border.ROUNDED, }
     '';
+
     keymap = {
       manager.prepend_keymap = [
         { on = "T"; run = "plugin --sync max-preview"; desc = "Maximize or restore preview"; }
