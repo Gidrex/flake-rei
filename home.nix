84f11241071d71a298853c2d81e4e17bfb26da2a
Alexander <Desench@proton.me>
2024-09-22 00:37:12 +0300

    better

--- home.nix
+++ home.nix
@@ -29,6 +29,29 @@
     # looking-glass-client.enable = true;
     lazygit.enable = true;
     direnv.enable = true;
+    wezterm = {
+      enable = true;
+      colorSchemes = {
+        myCoolTheme = {
+          ansi = [
+            "#222222" "#D14949" "#48874F" "#AFA75A"
+            "#599797" "#8F6089" "#5C9FA8" "#8C8C8C"
+          ];
+          brights = [
+            "#444444" "#FF6D6D" "#89FF95" "#FFF484"
+            "#97DDFF" "#FDAAF2" "#85F5DA" "#E9E9E9"
+          ];
+          background = "#1B1B1B";
+          cursor_bg = "#BEAF8A";
+          cursor_border = "#BEAF8A";
+          cursor_fg = "#1B1B1B";
+          foreground = "#BEAF8A";
+          selection_bg = "#444444";
+          selection_fg = "#E9E9E9";
+        };
+
+      };
+    };
 
     btop = {
       enable = true;
