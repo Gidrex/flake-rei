621b72cb13068afd3ca1befaf2cc5f1ebf346777
Alexander <Desench@proton.me>
2024-09-21 23:17:21 +0300

    better

--- modules/yazi/default.nix
+++ modules/yazi/default.nix
@@ -17,6 +17,7 @@ in
     extra-substituters = [ "https://yazi.cachix.org" ];
     extra-trusted-public-keys = [ "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k=" ];
   };
+  nixpkgs.overlays = [ yazi.overlays.default ];
 
   programs.yazi = {
     enable = true;
