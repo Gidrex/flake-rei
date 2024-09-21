c0054ea7ff67f07f2411824e8f8be25f859de190
Alexander <Desench@proton.me>
2024-09-21 22:15:28 +0300

    better

--- modules/yazi/default.nix
+++ modules/yazi/default.nix
@@ -1,19 +1,21 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   programs.yazi = {
     enable = true;
     shellWrapperName = "y";
+
     plugins = {
       full-border = pkgs.fetchFromGitHub {
         owner = "yazi-rs";
         repo = "plugins";
         rev = "main";
-        sha256 = "3F7RIg2CZH/jo+XhG0n4Zfspgi/77Hve421j0p3Og+Q=";
+        sha256 = lib.fakeSha256;
       } + "/full-border.yazi";
     };
+
     initLua = ''
       -- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
-        require("full-border"):setup { type = ui.Border.ROUNDED, }
+        require("full-border"):setup { type = ui.Border.PLAIN, }
     '';
   };
 }
