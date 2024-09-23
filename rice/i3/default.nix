90ee4977344b019b2f4258385bb715f039d50409
Alexander <Desench@proton.me>
2024-09-23 21:20:52 +0300

    better

--- rice/i3/default.nix
+++ rice/i3/default.nix
@@ -1,13 +1,6 @@
 { pkgs, ... }:
 {
   # i3 + xfce
-
-  environment.systemPackages = with pkgs; [
-    libexecinfo
-    i3 i3lock-fancy
-    gxkb parcellite
-  ];
-
   services.xserver = {
     windowManager.i3.enable = true;
     windowManager.i3.extraPackages = with pkgs; [ kitty i3status i3blocks ];
