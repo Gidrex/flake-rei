63a05abcf7c4347a4253b5732e5c67b29614d84b
Alexander <Desench@proton.me>
2024-09-23 21:21:13 +0300

    better

--- rice/i3/default.nix
+++ rice/i3/default.nix
@@ -3,7 +3,7 @@
   # i3 + xfce
   services.xserver = {
     windowManager.i3.enable = true;
-    windowManager.i3.extraPackages = with pkgs; [ kitty i3status i3lock i3blocks ];
+    windowManager.i3.extraPackages = with pkgs; [ kitty i3status i3blocks ];
     desktopManager.xterm.enable = false;
   };
 }
