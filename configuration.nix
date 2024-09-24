41c4465278bc091a34f7edbdbc79319c2697a2fa
Alexander <Desench@proton.me>
2024-09-24 15:58:02 +0300

    better

--- configuration.nix
+++ configuration.nix
@@ -23,7 +23,7 @@
 
   # Rice
   services = {
-    displayManager.defaultSession = "xfce+i3"; # "none+i3"
+    displayManager.defaultSession = "none+i3"; # "none+i3"
     displayManager.enable = true;
     xserver = {
       enable = true;
