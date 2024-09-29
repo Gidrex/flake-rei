4eeb733e30c19e8bfab11724b848f9811ed46839
Alexander <Desench@proton.me>
2024-09-29 19:57:25 +0300

    better

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
     # Users pkgs(flakes)
     byedpi.url = "github:Gidrex/byedpi-nix";
     catppuccin.url = "github:catppuccin/nix";
-    inputs.ayugram-desktop.url = "github:kaeeraa/ayugram-desktop/release?submodules=1";
+    ayugram-desktop.url = "github:kaeeraa/ayugram-desktop/release?submodules=1";
   };
 
   outputs = { nixpkgs, home-manager, catppuccin, byedpi, ayugram-desktop, ... }: 
