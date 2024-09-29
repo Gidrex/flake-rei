63f1bbbdca7b92707a19cfed226b1bd31b305fea
Alexander <Desench@proton.me>
2024-09-29 20:07:44 +0300

    better

--- configuration.nix
+++ configuration.nix
@@ -19,11 +19,6 @@
       grub.device = "nodev";
     };
   };
-  nix.settings = {
-    experimental-features = ["nix-command" "flakes"];
-    substituters = [ "https://cache.garnix.io" ];
-    trusted-public-keys = [ "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=" ];
-  };
 
   # Rice
   services = {
