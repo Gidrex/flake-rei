87086a29a88b2be8449f6810624b7094f94e1c7e
Alexander <Desench@proton.me>
2024-10-04 22:44:39 +0300

    better

--- flake.nix
+++ flake.nix
@@ -15,9 +15,10 @@
     byedpi.url = "github:Gidrex/byedpi-nix";
     catppuccin.url = "github:catppuccin/nix";
     ayugram-desktop.url = "github:kaeeraa/ayugram-desktop/release?submodules=1";
+    bluetui.url = "https://github.com/pythops/bluetui";
   };
 
-  outputs = { nixpkgs, home-manager, catppuccin, byedpi, ayugram-desktop, ... }: 
+  outputs = { nixpkgs, home-manager, catppuccin, byedpi, ayugram-desktop, bluetui, ... }: 
     let
       system = "x86_64-linux";
     in {
@@ -42,6 +43,7 @@
               environment.systemPackages = [
                 byedpi.packages.${system}.default
                 ayugram-desktop.packages.${system}.default
+                bluetui.packages.${system}.default
               ];
             }
           ];
