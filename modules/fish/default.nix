3455411b6d38c9ec774936a3d81c8fd7cd63e7d9
Alexander <Desench@proton.me>
2024-10-05 16:54:25 +0300

    better

--- modules/fish/default.nix
+++ modules/fish/default.nix
@@ -93,26 +93,35 @@
       functions = {
         search_files = {
           body = ''
-            set -l options "screenfetch 2>/dev/null" \
+          function search_files
+              set dir (zoxide query -l | fzf --height 40% --prompt="Directory: ") 
+              if test -z "$dir"
+                  return
+              end
+
+              set file (find "$dir" -type f -print 2>/dev/null | fzf --preview "bat {}" --preview-window=right:50%:wrap)
+              if test -z "$file"
+                  return
+              end
+              # z "$dir"
+              nvim "$file"
+          end
+          '';
+        };
+        fetch = {
+          body = ''
+            function fetch
+                set -l options "screenfetch 2>/dev/null" \
                                "fastfetch" \
                                "ghfetch -u Gidrex -c magenta --access-token (pass show github_token)" \
                                "starfetch -c magenta"
-
                 set -l selected_command (printf "%s\n" $options | fzf --height 40% --reverse)
-
                 if test -n "$selected_command"
                     eval $selected_command
                 else
                     echo "Команда не выбрана."
                 end
             end
-          '';
-        };
-        fetch = {
-          body = ''
-
-
-      starfetch -c magenta
           '';
         };
       };
