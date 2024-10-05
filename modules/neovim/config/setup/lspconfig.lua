5f7b01ee1908eaad7bbcbf32ca9523be2601e831
Alexander <Desench@proton.me>
2024-10-05 16:08:55 +0300

    better

--- modules/neovim/config/setup/lspconfig.lua
+++ modules/neovim/config/setup/lspconfig.lua
@@ -25,8 +25,8 @@ require'lspconfig'.pyright.setup{
   }
 }
 require'lspconfig'.vimls.setup{}
-require'lspconfig'.eslint.setup{ cmd = { "eslint_d" }, capabilities = capabilities, }
-require'lspconfig'.tsserver.setup{capabilities=capabilities}
+-- require'lspconfig'.eslint.setup{ cmd = { "eslint_d" }, capabilities = capabilities, }
+-- require'lspconfig'.tsserver.setup{capabilities=capabilities}
 require'lspconfig'.gradle_ls.setup{capabilities=capabilities}
 
 -- Global mappings.
