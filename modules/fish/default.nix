c6470ab68d2b8f08c46de8de2484ac2ede1efd21
Alexander <Desench@proton.me>
2024-10-05 16:57:36 +0300

    better

--- modules/fish/default.nix
+++ modules/fish/default.nix
@@ -43,11 +43,6 @@
         nix-shell-init = "touch default.nix && echo '{ pkgs ? import <nixpkgs> {} }:' > default.nix && echo '' >> default.nix && echo 'pkgs.mkShell {' >> default.nix && echo '  buildInputs = with pkgs; [' >> default.nix && echo '    # add your own packets' >> default.nix && echo '  ];' >> default.nix && echo '}' >> default.nix";
         wine-ru = "LANG=ru_RU.UTF-8 LC_ALL=ru_RU.UTF-8 wine '$@'";
         copy = "xclip -selection clipboard";
-        fetch = ''echo -e "
-          screenfetch 2>/dev/null
-          fastfetch
-          ghfetch -u Gidrex -c magenta --access-token $(pass show github_token)
-        " | fzf | sh'';
         browser = "io.github.zen_browser.zen";
 
         # custom fzf scripts
@@ -124,7 +119,7 @@
                     case "starfetch"
                         starfetch -c magenta
                     case '*'
-                        echo "Error"
+                        echo "Error."
                 end
             end
           '';
