cc4d4b8a3c500766f5f4b4ef95c1752692b94828
Alexander <Desench@proton.me>
2024-10-07 01:04:55 +0300

    better

--- modules/prismlauncher/default.nix
+++ modules/prismlauncher/default.nix
@@ -1,11 +1,11 @@
 { pkgs, ... }:
 let
-  prismLauncherThemes = builtins.fetchGit {
+  Themes = builtins.fetchGit {
     url = "https://github.com/PrismLauncher/Themes.git";
     rev = "main";
     sha256 = "Jzpb9fgyPYa1NxgFBy8HnHtfy8nORNS4jzMUzNEDFJ8=";
   };
 in {
   home.packages = with pkgs; [ prismlauncher ];
- home.file.".local/share/PrismLauncher/Themes".source = prismLauncherThemes;
+ home.file.".local/share/PrismLauncher/Themes".source = Themes;
 }
