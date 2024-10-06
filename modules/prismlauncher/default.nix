26de99723acfa4afdc4509f377d0c79e79cd9ebc
Alexander <Desench@proton.me>
2024-10-07 01:07:42 +0300

    better

--- modules/prismlauncher/default.nix
+++ modules/prismlauncher/default.nix
@@ -3,9 +3,10 @@ let
   Themes = pkgs.fetchFromGitHub {
     owner = "PrismLauncher";
     repo = "Themes";
-    rev = "0a1fcdba369fa1cccdfdc83aa495b9b239e869b0";
+    rev = "main";
     sha256 = "Jzpb9fgyPYa1NxgFBy8HnHtfy8nORNS4jzMUzNEDFJ8=";
   };
+
 in {
   home.packages = with pkgs; [ prismlauncher ];
   home.file.".local/share/PrismLauncher/Themes".source = Themes;
