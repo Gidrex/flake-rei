f046058a040182edc621b4936bee9824a86258e0
Alexander <Desench@proton.me>
2024-10-07 01:44:24 +0300

    better

--- modules/prismlauncher/default.nix
+++ modules/prismlauncher/default.nix
@@ -7,14 +7,27 @@ let
     sha256 = "Jzpb9fgyPYa1NxgFBy8HnHtfy8nORNS4jzMUzNEDFJ8=";
   };
 
+  copyThemes = ''
+    PRISM_DIR="${config.home.homeDirectory}/.local/share/PrismLauncher"
+
+    cp -rn ${Themes}/{icons,themes} "$PRISM_DIR/"
+    cp -rn ${Themes}/cats/* "$PRISM_DIR/catpacks/"
+  '';
+
+  updateConfig = ''
+    PRISM_DIR="${config.home.homeDirectory}/.local/share/PrismLauncher"
+    MY_CONFIG=./myprism.cfg
+    TARGET_CONFIG="$PRISM_DIR/prismlauncher.cfg"
+
+    awk -F '=' 'NR==FNR { a[$1]=$2; next } $1 in a { $2=a[$1] }1' OFS='=' "$MY_CONFIG" "$TARGET_CONFIG" > "$TARGET_CONFIG.tmp"
+    mv "$TARGET_CONFIG.tmp" "$TARGET_CONFIG"
+  '';
+
 in {
   home.packages = with pkgs; [ prismlauncher ];
 
-  home.activation.copyPrismLauncherThemes = ''
-    PRISM_DIR="${config.home.homeDirectory}/.local/share/PrismLauncher"
-
-    cp -rn ${Themes}/icons "$PRISM_DIR/"
-    cp -rn ${Themes}/themes "$PRISM_DIR/"
-    cp -rn ${Themes}/cats/* "$PRISM_DIR/catpacks/"
-  '';
+  home.activation = {
+    copyPrismLauncherThemes = copyThemes;
+    updatePrismLauncherConfig = updateConfig;
+  };
 }
