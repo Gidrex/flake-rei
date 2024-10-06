8eff4365200e83d39adeb12b447de0cad5d0b6f7
Alexander <Desench@proton.me>
2024-10-07 01:46:42 +0300

    better

--- modules/prismlauncher/default.nix
+++ modules/prismlauncher/default.nix
@@ -11,9 +11,14 @@ let
     PRISM_DIR="${config.home.homeDirectory}/.local/share/PrismLauncher"
 
     mkdir -p "$PRISM_DIR/catpacks"
-    cp -r ${Themes}/icons "$PRISM_DIR/"
-    cp -r ${Themes}/themes "$PRISM_DIR/"
-    cp -r ${Themes}/cats/* "$PRISM_DIR/catpacks/"
+    if [ -w "$PRISM_DIR" ]; then
+      cp -r ${Themes}/icons "$PRISM_DIR/"
+      cp -r ${Themes}/themes "$PRISM_DIR/"
+      cp -r ${Themes}/cats/* "$PRISM_DIR/catpacks/"
+    else
+      echo "Permission denied: Cannot write to $PRISM_DIR"
+      exit 1
+    fi
   '';
 
   updateConfig = ''
@@ -21,7 +26,12 @@ let
     MY_CONFIG=./myprism.cfg
     TARGET_CONFIG="$PRISM_DIR/prismlauncher.cfg"
 
-    awk -F '=' 'NR==FNR { a[$1]=$2; next } $1 in a { $2=a[$1] }1' OFS='=' "$MY_CONFIG" "$TARGET_CONFIG" > "$TARGET_CONFIG.tmp" && mv "$TARGET_CONFIG.tmp" "$TARGET_CONFIG"
+    if [ -w "$TARGET_CONFIG" ]; then
+      awk -F '=' 'NR==FNR { a[$1]=$2; next } $1 in a { $2=a[$1] }1' OFS='=' "$MY_CONFIG" "$TARGET_CONFIG" > "$TARGET_CONFIG.tmp" && mv "$TARGET_CONFIG.tmp" "$TARGET_CONFIG"
+    else
+      echo "Permission denied: Cannot write to $TARGET_CONFIG"
+      exit 1
+    fi
   '';
 
 in {
