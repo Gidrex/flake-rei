2c0b849ccf5d15f6746aebdaa5ab01e88a6d0ac3
Alexander <Desench@proton.me>
2024-10-07 01:19:21 +0300

    better

--- modules/prismlauncher/default.nix
+++ modules/prismlauncher/default.nix
@@ -13,6 +13,5 @@ in {
   home.activation.copyPrismLauncherThemes = ''
     cp -r ${Themes}/icons"${config.home.homeDirectory}/.local/share/PrismLauncher/"
     cp -r ${Themes}/themes"${config.home.homeDirectory}/.local/share/PrismLauncher/"
-    cp -r ${Themes}/cats"${config.home.homeDirectory}/.local/share/PrismLauncher/"
   '';
 }
