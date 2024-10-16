12893ae6982989d8c1f59430784c09ab330d970a
Alexander <Desench@proton.me>
2024-10-16 22:11:00 +0300

    better

--- packages/appimage/default.nix
+++ packages/appimage/default.nix
@@ -1,19 +1,25 @@
 { pkgs, ... }:
 let
+  # Icons
   logseq-icon = pkgs.fetchurl {
     url = "https://raw.githubusercontent.com/logseq/logseq/refs/heads/master/resources/icon.png";
     sha256 = "1jg24pi88nl8ynk2zbmz4qkl2al38w28kjv8drddgi8vvqh76c77";
   };
-
-zen-icon = pkgs.fetchurl {
+  zen-icon = pkgs.fetchurl {
     url = "https://raw.githubusercontent.com/zen-browser/desktop/6d0753c5e9e054a420c28dc207220c5ff4694a9d/docs/assets/zen-black.svg";
     sha256 = "1jg24pi88nl8ynk2zbmz4qkl2al38w28kjv8drddgi8vvqh76c77";
   };
+
+  # Packages
+  logseq = pkgs.callPackage ./logseq.nix {};
+  zenBrowser = pkgs.callPackage ./zen-browser.nix {};
+
+  # home-manager
 in
-  {
+{
   home.packages = with pkgs; [
-    (callPackage ./logseq.nix {})
-    (callPackage ./zen-browser.nix {})
+    logseq
+    zenBrowser
   ];
 
   xdg.desktopEntries = {
@@ -21,7 +27,7 @@ in
       name = "Logseq";
       genericName = "Knowledge Management Tool";
       comment = "Open-source knowledge management and collaboration tool";
-      exec = "logseq";
+      exec = "${logseq}/bin/logseq";
       icon = "${logseq-icon}";
       terminal = false; 
       type = "Application";
@@ -30,19 +36,17 @@ in
       startupNotify = true;
     };
 
-    xdg.desktopEntries = {
-      zenbrowser = {
-        name = "Zen Browser";
-        genericName = "Privacy-Focused Web Browser";
-        comment = "Beautifully designed, privacy-focused, and packed with features.";
-        exec = "${pkgs.zen-browser}/bin/zen-generic";
-        icon = "${zen-icon}";
-        terminal = false; 
-        type = "Application";
-        categories = [ "Network" "WebBrowser" ];
-        mimeType = [ "application/x-zenbrowser" ];
-        startupNotify = true;
-      };
+    zenbrowser = {
+      name = "Zen Browser";
+      genericName = "Privacy-Focused Web Browser";
+      comment = "Beautifully designed, privacy-focused, and packed with features.";
+      exec = "${zenBrowser}/bin/zen-generic";
+      icon = "${zen-icon}";
+      terminal = false; 
+      type = "Application";
+      categories = [ "Network" "WebBrowser" ];
+      mimeType = [ "application/x-zenbrowser" ];
+      startupNotify = true;
     };
   };
 }
