5b2aee8628f8ce37a9c2d8d346593869c22f1260
Alexander <Desench@proton.me>
2024-10-16 21:04:00 +0300

    better

--- packages/appimage/default.nix
+++ packages/appimage/default.nix
@@ -1,4 +1,10 @@
 { pkgs, ... }:
+ let
+    icon = pkgs.fetchurl {
+      url = "https://github.com/logseq/logseq/raw/master/resources/icon.png";
+      sha256 = "1jg24pi88nl8ynk2zbmz4qkl2al38w28kjv8drddgi8vvqh76c77";
+    };
+  in
 {
   home.packages = with pkgs; [
     (callPackage ./logseq.nix {})
