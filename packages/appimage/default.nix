33fc83c4899c6d1813d4ac21178ee2e9438c9861
Alexander <Desench@proton.me>
2024-10-16 21:07:23 +0300

    better

--- packages/appimage/default.nix
+++ packages/appimage/default.nix
@@ -1,21 +1,21 @@
 { pkgs, ... }:
- let
-    logseq-icon = pkgs.fetchurl {
-      url = "https://github.com/logseq/logseq/raw/master/resources/icon.png";
-      sha256 = "1jg24pi88nl8ynk2zbmz4qkl2al38w28kjv8drddgi8vvqh76c77";
-    };
-  in
-{
+let
+  logseq-icon = pkgs.fetchurl {
+    url = "https://github.com/logseq/logseq/raw/master/resources/icon.png";
+    sha256 = "1jg24pi88nl8ynk2zbmz4qkl2al38w28kjv8drddgi8vvqh76c77";
+  };
+in
+  {
   home.packages = with pkgs; [
     (callPackage ./logseq.nix {})
   ];
   xdg.desktopEntries = {
-logseq = {
+    logseq = {
       name = "Logseq";
       genericName = "Knowledge Management Tool";
       comment = "Open-source knowledge management and collaboration tool";
       exec = "logseq";
-      icon = loseq-icon;
+      icon = ${loseq-icon};
       terminal = false; 
       type = "Application";
       categories = [ "Utility" ];
