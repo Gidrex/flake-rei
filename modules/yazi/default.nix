68cd241d0c1833ae003f6876ba1ab068c82c32d1
Alexander <Desench@proton.me>
2024-10-18 12:50:50 +0300

    better

--- modules/yazi/default.nix
+++ modules/yazi/default.nix
@@ -41,7 +41,7 @@ in
         { on = [ "g" "r" ]; run = ''shell 'ya pub dds-cd --str "$(git rev-parse --show-toplevel)"' --confirm''; desc = "back to the root of repository"; }
         { on   = [ "e" "t" ]; run  = "plugin --sync hide-preview"; desc = "Hide or show preview"; }
         { on = [ "e" "d" ]; run  = ''shell 'dragon -T "$@"' --confirm''; desc = "Drag & Drop file"; }
-        { on = ["e" "e" ]; run = "shell '$@'"; desc = "Open selected file with custom command"; }
+        { on = ["e" "e" ]; run = "shell '$@' --confirm"; desc = "Open selected file with custom command"; }
       ];
     };
   };
