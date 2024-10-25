dc1fbb368710e0587a82a726f92ba13e53b2b135
Alexander <Desench@proton.me>
2024-10-26 00:05:14 +0300

    better

--- home.nix
+++ home.nix
@@ -1,5 +1,5 @@
 {  pkgs, ... }:
-{
+  {
   imports = [
     ./modules/neovim
     ./modules/tmux
@@ -39,120 +39,121 @@
 
     lazygit = {
       enable = true;
-      settings.gui = {
-        nerdFontsVersion = 3;
+      settings = {
+        gui = {
+          nerdFontsVersion = 3;
+        };
+      };
+
+      gh = {
+        enable = true;
+        settings.git_protocol = "ssh";
+        extensions = with pkgs; [ gh-s gh-copilot ];
+      };
+
+      btop = {
+        enable = true;
+        extraConfig = ''color_theme = "Default"'';
+      };
+
+      fzf = {
+        enable = true;
+        # defaultOptions = [ "--preview 'bat {}'" ];
+        defaultCommand = "fd --type f";
+      };
+
+      pandoc = {
+        enable = true;
+        defaults.metadata.author = "Alexander";
+        defaults.pdf-engine = "xelatex";
+        defaults.citeproc = true;
+      };
+
+      eza = {
+        enable = true;
+        icons = true;
+        git = true;
+        extraOptions = [ "--oneline" "--group-directories-first" ];
+      };
+
+      bat = {
+        enable = true;
+        config.style = "numbers";
+        catppuccin.enable = true;
+        # config.theme = "Nord";
+        config.pager = "less -FR";
+        config.color = "always";
+      };
+
+      git = {
+        enable = true;
+        userName = "Alexander";
+        userEmail = "Desench@proton.me";
+        extraConfig = {
+          init.defaultBranch = "main";
+          pull.rebase = true;
+          rebase.autostash = true;
+          rebase.autosquash = true;
+          push.autoSetupRemote = true;
+          commit.gpgsign = false;
+          rerere.enabled = true;
+          gpg.format = "ssh";
+          user.signingkey = "~/.ssh/id_rsa.pub";
+          core.whitespace = "trailing-space,space-before-tab";
+          core.editor = "nvim";
+        };
       };
     };
 
-    gh = {
-      enable = true;
-      settings.git_protocol = "ssh";
-      extensions = with pkgs; [ gh-s gh-copilot ];
-    };
+    # Services
+    services = {
+      spotifyd.enable = true;
+      spotifyd.settings.global = {
+        device_name = "nix";
+        username = "Gidrex";
+        password_cmd = "pass show spotifyd";
+        backend = "alsa";
+        dbus_type = "session"; # "system" for no graphic
+        bitrate = 320;
+        volume_normalisation = true;
+        autoplay = true;
+        proxy = "http://127.0.0.1:8889";
+      };
 
-    btop = {
-      enable = true;
-      extraConfig = ''color_theme = "Default"'';
-    };
-
-    fzf = {
-      enable = true;
-      # defaultOptions = [ "--preview 'bat {}'" ];
-      defaultCommand = "fd --type f";
-    };
-
-    pandoc = {
-      enable = true;
-      defaults.metadata.author = "Alexander";
-      defaults.pdf-engine = "xelatex";
-      defaults.citeproc = true;
-    };
-
-    eza = {
-      enable = true;
-      icons = true;
-      git = true;
-      extraOptions = [ "--oneline" "--group-directories-first" ];
-    };
-
-    bat = {
-      enable = true;
-      config.style = "numbers";
-      catppuccin.enable = true;
-      # config.theme = "Nord";
-      config.pager = "less -FR";
-      config.color = "always";
-    };
-
-    git = {
-      enable = true;
-      userName = "Alexander";
-      userEmail = "Desench@proton.me";
-      extraConfig = {
-        init.defaultBranch = "main";
-        pull.rebase = true;
-        rebase.autostash = true;
-        rebase.autosquash = true;
-        push.autoSetupRemote = true;
-        commit.gpgsign = false;
-        rerere.enabled = true;
-        gpg.format = "ssh";
-        user.signingkey = "~/.ssh/id_rsa.pub";
-        core.whitespace = "trailing-space,space-before-tab";
-        core.editor = "nvim";
+      udiskie = {
+        enable = true;
+        tray = "auto";
+        automount = true;
       };
     };
-  };
 
-  # Services
-  services = {
-    spotifyd.enable = true;
-    spotifyd.settings.global = {
-      device_name = "nix";
-      username = "Gidrex";
-      password_cmd = "pass show spotifyd";
-      backend = "alsa";
-      dbus_type = "session"; # "system" for no graphic
-      bitrate = 320;
-      volume_normalisation = true;
-      autoplay = true;
-      proxy = "http://127.0.0.1:8889";
+    # home-manager options
+    programs.home-manager.enable = true;
+    home = { 
+      # Packages
+      packages = with pkgs; [
+
+        # theming
+        tokyo-night-gtk
+        catppuccin
+        catppuccin-kvantum
+      ];
+
+      sessionVariables = {
+        EDITOR = "nvim";
+        VISUAL = "nvim";
+        TERM = "alacritty";
+        CHROME_EXECUTABLE = "/run/current-system/sw/bin/chromium";
+        # QT_QPA_PLATFORMTHEME = "qt5ct";
+      };
+
+      sessionPath = [
+        "$HOME/.cargo/bin"
+        "$HOME/go/bin"
+        "/usr/bin"
+      ];
+
+      enableNixpkgsReleaseCheck = false;
+      stateVersion = "24.05";
     };
-
-    udiskie = {
-      enable = true;
-      tray = "auto";
-      automount = true;
-    };
-  };
-
-  # home-manager options
-  programs.home-manager.enable = true;
-  home = { 
-    # Packages
-    packages = with pkgs; [
-
-      # theming
-      tokyo-night-gtk
-      catppuccin
-      catppuccin-kvantum
-    ];
-
-    sessionVariables = {
-      EDITOR = "nvim";
-      VISUAL = "nvim";
-      TERM = "alacritty";
-      CHROME_EXECUTABLE = "/run/current-system/sw/bin/chromium";
-      # QT_QPA_PLATFORMTHEME = "qt5ct";
-    };
-
-    sessionPath = [
-      "$HOME/.cargo/bin"
-      "$HOME/go/bin"
-      "/usr/bin"
-    ];
-
-    enableNixpkgsReleaseCheck = false;
-    stateVersion = "24.05";
-  };
-}
+  }
