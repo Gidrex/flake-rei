c29b17cc562cca530911030d110530556416bc46
Alexander <Desench@proton.me>
2024-11-06 12:57:03 +0300

    better

--- modules/fish/default.nix
+++ modules/fish/default.nix
@@ -66,10 +66,10 @@
       search_files
       fetch
 
-      set -x http_proxy http://127.0.0.1:8889
-      set -x https_proxy http://127.0.0.1:8889
-      set -x all_proxy socks5://localhost:1089
-      set -x no_proxy localhost,127.0.0.1,::1
+      # set -x http_proxy http://127.0.0.1:8889
+      # set -x https_proxy http://127.0.0.1:8889
+      # set -x all_proxy socks5://localhost:1089
+      # set -x no_proxy localhost,127.0.0.1,::1
 
       set -Ux fifc_editor nvim
       set -U fifc_exa_opts  --oneline --icons --git --tree --level 2
