c3d4698d8454d1f72078901e78b6a554184d3e18
Alexander <Desench@proton.me>
2024-11-12 23:11:52 +0300

    better

--- configuration.nix
+++ configuration.nix
@@ -250,7 +250,7 @@
     openssl
     cloak pass sshpass
     shadowsocks-rust shadowsocks-libev 
-    wireguard-tools udp2raw inetutils v2ray
+    openvpn udp2raw inetutils v2ray
     crow-translate
     ytfzf yt-dlp 
     git-filter-repo
