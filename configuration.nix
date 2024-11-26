8fc2ad9a09268129eeded7ecedf844d2ccc1ae72
Alexander <Desench@proton.me>
2024-11-27 00:48:37 +0300

    better

--- configuration.nix
+++ configuration.nix
@@ -120,8 +120,8 @@
 
   # Locale
   time.timeZone = "Europe/Moscow";
-  i18n.defaultLocale = "en_GB.UTF-8";
-  i18n.supportedLocales = [ "ru_RU.UTF-8/UTF-8" "en_GB.UTF-8/UTF-8" ];  
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.supportedLocales = [ "ru_RU.UTF-8/UTF-8" "en_US.UTF-8" "en_GB.UTF-8" ];  
 
   # User
   users.users.gidrex = {
@@ -458,6 +458,10 @@
   '';
   environment.variables = {
     SDL_GAMECONTROLLERCONFIG = ''0500d71f7e0500000920000001800000,Nintendo Switch Pro Controller,a:b1,b:b0,back:b9,dpdown:h0.4,dpleft:h0.8,dpright:h0.2,dpup:h0.1,guide:b11,leftshoulder:b5,leftstick:b12,lefttrigger:b7,leftx:a0,lefty:a1,rightshoulder:b6,rightstick:b13,righttrigger:b8,rightx:a2,righty:a3,start:b10,x:b2,y:b3,hint:SDL_GAMECONTROLLER_USE_BUTTON_LABELS:=1,platform:Linux'';
+    SDL_HINT_JOYSTICK_HIDAPI = "1";
+    SDL_HINT_JOYSTICK_HIDAPI_SWITCH = "1";
+    SDL_HINT_JOYSTICK_HIDAPI_JOY_CONS = "0";
+    SDL_HINT_JOYSTICK_HIDAPI_GAMECUBE = "0";
   };
 
   system.stateVersion = "24.11";
