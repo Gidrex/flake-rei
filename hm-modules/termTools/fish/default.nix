f970452dfca597ee0fb7c504d95abef6198a2cf0
Gidrex <Desench@proton.me>
2025-12-25 17:06:24 +0300

    change claude->gemini

--- hm-modules/termTools/fish/default.nix
+++ hm-modules/termTools/fish/default.nix
@@ -58,7 +58,7 @@ in {
       bind -M insert \et 'commandline -f cancel; z ..; commandline -f repaint'
       bind -M insert \eh 'commandline -f cancel; helixing; commandline -f repaint'
       bind -M insert \ey 'commandline -f cancel; yazi; commandline -f repaint'
-      bind -M insert \ea 'commandline -f cancel; claude "/resume"; commandline -f repaint'
+      bind -M insert \ea 'commandline -f cancel; gemini; commandline -f repaint'
       bind -M insert \ex 'commandline -f cancel; lazygit; commandline -f repaint'
     '';
     loginShellInit = ''
