99f4dec065fbbce52550ec965591a5df075ca507
Gidrex <Desench@proton.me>
2026-02-03 15:48:49 +0300

    add OCR script

--- assets/scripts/OCR_select_area.sh
+++ assets/scripts/OCR_select_area.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
+LANG=$(echo -e "rus\neng" | wofi --width "80px" --prompt "Lang..." --height "200px" --style /home/gidrex/flake-rei/dotfiles/niri/wofi/style.css -S dmenu)
+
+if [ -z "$LANG" ]; then
+  exit 0
+fi
+
 grim -g "$(slurp)" - | \
   magick - \
   -alpha set -background white -alpha remove \
   -modulate 100,0 \
-  -resize 400% \
+  -resize 300% \
   -density 300 \
   -bordercolor white -border 50x50 \
   -sharpen 0x1.5 \
   png:- | \
-  tesseract - - -l rus+eng --psm 7 | wl-copy
+  tesseract - - -l "$LANG" | wl-copy
